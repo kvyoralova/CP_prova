c003be8909d394fb6d213894246c9d59eb84784a
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-03-21 14:58:16 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,2 +1,2 @@
-import streramlit as st #'import _ as _' means that you're "assigning" a shorter "variable" to the library
+import streamlit as st #'import _ as _' means that you're "assigning" a shorter "variable" to the library
 st.text("hello world") #the text function displays the text between quotes and brackets
