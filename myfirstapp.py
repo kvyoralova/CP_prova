8384396f5f815ed2b8cc98713e959d9d1d29a944
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-03-21 14:48:21 +0100

    Create myfirstapp.py
    Strumentopolo misterioso

---
+++ myfirstapp.py
@@ -0,0 +1,2 @@
+import streramlit as st #'import _ as _' means that you're "assigning" a shorter "variable" to the library
+st.text("hello world") #the text function displays the text between quotes and brackets
