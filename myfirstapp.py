2c245aa986f7306037435ccfae831d815452a71b
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-03-21 15:32:32 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -1,2 +1,3 @@
 import streamlit as st #'import _ as _' means that you're "assigning" a shorter "variable" to the library
-st.text("hello world") #the text function displays the text between quotes and brackets
+st.header("hello world") #the text function displays the text between quotes and brackets
+st.text("from brixen")
