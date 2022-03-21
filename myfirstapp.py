9f156736719f82a436cc5c5683e70047f416de8a
kvyoralova <101127512+kvyoralova@users.noreply.github.com>
2022-03-21 15:47:08 +0100

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -2,3 +2,9 @@ import streamlit as st #'import _ as _' means that you're "assigning" a shorter 
 st.header("Hello world") #the text function displays the text between quotes and brackets
 title = st.text_input("Gimme a movie title:")
 st.write("The current movie title is ", title)
+
+genre = st.radio("What's your favorite movie genre",('Comedy', 'Drama', 'Documentary'))
+if genre == 'Comedy':
+     st.write('You selected comedy.')
+ else:
+     st.write("You didn't select comedy.")
